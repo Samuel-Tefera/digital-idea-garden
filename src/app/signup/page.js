3e610278e930dbc4efa24818753ee9f747ca1213
Samuel-Tefera <samuelteferab@gmail.com>
2025-08-10 23:35:47 +0300

    build user signup form

---
+++ src/app/signup/page.js
@@ -0,0 +1,70 @@
+import Link from 'next/link';
+import Button from '../_components/Button';
+import Image from 'next/image';
+
+export const metadata = {
+  title: 'Sign Up',
+};
+
+export default function Page() {
+  return (
+    <div className="m-auto mt-20 w-[24rem] rounded-lg border p-8 px-8 text-primary-950 shadow-lg">
+      <h1 className="mb-2 text-center text-2xl font-semibold">
+        Create Account
+      </h1>
+      <p className="mb-4 text-center text-sm">
+        Hey, Enter your details to create your account
+      </p>
+      <form>
+        <div className="mb-4">
+          <input
+            className="w-full rounded-md border border-neutral-400 p-2"
+            type="text"
+            name="fullname"
+            placeholder="Full Name"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            className="w-full rounded-md border border-neutral-400 p-2"
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            className="w-full rounded-md border border-neutral-400 p-2"
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+        </div>
+        <Button>Sign Up</Button>
+      </form>
+      <p className="my-2 text-center text-sm">Or Sign up with</p>
+      <form>
+        <button className="flex w-full items-center justify-center gap-2 rounded-md border border-primary-300 px-6 py-2 font-semibold text-primary-950">
+          <Image
+            src="https://authjs.dev/img/providers/google.svg"
+            alt="Google logo"
+            height="18"
+            width="18"
+          />
+          <span>Google</span>
+        </button>
+      </form>
+      <div className="mt-2 text-center text-sm">
+        <p>
+          Already have an account?
+          <Link
+            className="text-primary-600 underline hover:text-primary-700"
+            href="#"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
