e00db3aa6b2b4d5d6c11938069de39041080df90
Samuel-Tefera <samuelteferab@gmail.com>
2025-08-18 14:49:52 +0300

    create sign up page

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,71 +1,9 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import Button from '../_components/Button';
-import { signInWithGoogleAction, signUpWithEmailAction } from '../_lib/actions';
+import SignUpForm from '../_components/SignUpForm';
 
 export const metadata = {
   title: 'Sign Up',
 };
 
-export default function Page() {
-  return (
-    <div className="m-auto mt-20 w-80 rounded-lg border border-neutral-700 p-8 text-primary-50 shadow-sm shadow-neutral-700 sm:w-[24rem]">
-      <h1 className="mb-2 text-center text-2xl font-semibold">
-        Create Account
-      </h1>
-      <p className="mb-4 text-center text-sm">
-        Hey, Enter your details to create your account
-      </p>
-      <form action={signUpWithEmailAction}>
-        <div className="mb-4">
-          <input
-            className="w-full rounded-md border border-neutral-600 bg-neutral-800 p-2 text-neutral-100"
-            type="text"
-            name="fullName"
-            placeholder="Full Name"
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            className="w-full rounded-md border border-neutral-600 bg-neutral-800 p-2 text-neutral-100"
-            type="email"
-            name="email"
-            placeholder="Email"
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            className="w-full rounded-md border border-neutral-600 bg-neutral-800 p-2 text-neutral-100"
-            type="password"
-            name="password"
-            placeholder="Password"
-          />
-        </div>
-        <Button type="submit">Sign Up</Button>
-      </form>
-      <p className="my-2 text-center text-sm">Or Sign up with</p>
-      <form action={signInWithGoogleAction}>
-        <button className="flex w-full items-center justify-center gap-2 rounded-md border border-neutral-600 px-6 py-2 font-semibold text-primary-50">
-          <Image
-            src="https://authjs.dev/img/providers/google.svg"
-            alt="Google logo"
-            height="18"
-            width="18"
-          />
-          <span>Google</span>
-        </button>
-      </form>
-      <div className="mt-2 text-center text-sm">
-        <p>
-          Already have an account?{' '}
-          <Link
-            className="text-primary-600 underline hover:text-primary-700"
-            href="/login"
-          >
-            Login
-          </Link>
-        </p>
-      </div>
-    </div>
-  );
+export default function SignUpPage() {
+  return <SignUpForm />;
 }
