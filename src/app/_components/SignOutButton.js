cfb26bb12065b58d469b14fbdaa665029278bdb5
Samuel-Tefera <samuelteferab@gmail.com>
2025-09-01 16:56:56 +0300

    handle signout request

--- src/app/_components/SignOutButton.js
+++ src/app/_components/SignOutButton.js
@@ -1,10 +1,22 @@
+'use client';
+
+import { signOut } from 'next-auth/react';
+import { useState } from 'react';
 import { FiLogOut } from 'react-icons/fi';
+import SpinnerMini from './SpinnerMini';
 
 function SignOutButton() {
+  const [isLogouting, setIsLogouting] = useState(false);
+
+  async function handleLogout() {
+    setIsLogouting(true);
+    await signOut({ redirectTo: '/' });
+  }
+
   return (
     <div className="hover:text-primary-400">
-      <button>
-        <FiLogOut />
+      <button onClick={handleLogout}>
+        {isLogouting ? <SpinnerMini /> : <FiLogOut />}
       </button>
     </div>
   );
